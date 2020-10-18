dad76aa8073043e6215ff3adc2d5f5bf46ab8d3a
Glitch (xn--11ty-4s83c) <none>
2020-10-18 03:11:06 +0000

    👆💧 Checkpoint ./package.json:8045597/1689 ./.eleventy.js:8045597/8899

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,7 @@ const embedEverything = require("eleventy-plugin-embed-everything");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 const htmlmin = require("html-minifier");
+const charts = require("eleventy-charts");
 
 module.exports = function(eleventyConfig) {
   // Add plugins
@@ -14,6 +15,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
   eleventyConfig.addPlugin(embedEverything);
+  eleventyConfig.addPlugin(charts);
 
   eleventyConfig.setDataDeepMerge(true);
   // Mimify HTML, CSS and JS files
